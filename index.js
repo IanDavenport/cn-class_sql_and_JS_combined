9945d39253ed92a666aa218cbaf1ce544cfff698
Ian Davenport <davenporti632@gmail.com>
2020-10-21 10:21:11 +0100

    First load

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+// npm i express
+
+const app = express(); 
+// initialised express function to use it thought the file
+
+const routes = require('./routes');
+// set up our routes so out app.get in aother file
+// keeping our index.js nice and clean 
+
+app.use('/', routes);
+
+app.listen(process.env.PORT || 3005, () => {
+    console.log('Server is listening.')
+})
+
+
+
+
+
+
+
+
